7743b34bc74873800f47f420dd0948b51851f396
giobrazil <toushiroshinigami@gmail.com>
2016-06-19 23:38:57 +0000

    Add files via upload

---
+++ sentiment_analysis.py
@@ -0,0 +1,160 @@
+###############################################################################
+# Univesidade Federal de Pernambuco -- UFPE (http://www.ufpe.br)
+# Centro de Informatica -- CIn (http://www.cin.ufpe.br)
+# Bacharelado em Sistemas de Informacao
+# IF968 -- Programacao 1
+#
+# Autor:    Fulano de Tal
+#            Beltrano do Cin
+#
+# Email:    fdt@cin.ufpe.br
+#            bdc@cin.ufpe.br
+#
+# Data:        2016-06-10
+#
+# Descricao:  Este e' um modelo de arquivo para ser utilizado para a implementacao
+#                do projeto pratico da disciplina de Programacao 1. 
+#                 A descricao do projeto encontra-se no site da disciplina e trata-se
+#                de uma adaptacao do projeto disponivel em 
+#                http://nifty.stanford.edu/2016/manley-urness-movie-review-sentiment/
+#                O objetivo deste projeto e' implementar um sistema de analise de
+#                sentimentos de comentarios de filmes postados no site Rotten Tomatoes.
+#
+# Licenca: The MIT License (MIT)
+#            Copyright(c) 2016 Fulano de Tal, Beltrano do Cin
+#
+###############################################################################
+
+import sys
+import re
+
+def clean_up(s):
+    ''' Retorna uma versao da string 's' na qual todas as letras sao
+        convertidas para minusculas e caracteres de pontuacao sao removidos
+        de ambos os extremos. A pontuacao presente no interior da string
+        e' mantida intacta.
+    '''    
+    punctuation = ''''!"',;:.-?)([]<>*#\n\t\r'''
+    result = s.lower().strip(punctuation)
+    return result
+
+
+def split_on_separators(original, separators):
+    '''    Retorna um vetor de strings nao vazias obtido a partir da quebra
+        da string original em qualquer dos caracteres contidos em 'separators'.
+        'separtors' e' uma string formada com caracteres unicos a serem usados
+        como separadores. Por exemplo, '^$' e' uma string valida, indicando que
+        a string original sera quebrada em '^' e '$'.
+    '''
+
+    ''' filter é um filtro e recebe como argumento a função e a lista de argumento para
+        ser filtrada. retorna os valores verdadeiros baseado na função fornecida
+    '''
+    return list(filter(lambda x: len(x) > 1,[clean_up(words) for words in re.split(separators,original) if words not in GetStopWords()]))
+                    
+
+
+def readTrainingSet(fname):
+    '''    Recebe o caminho do arquivo com o conjunto de treinamento como parametro
+        e retorna um dicionario com triplas (palavra,freq,escore) com o escore
+        medio das palavras no comentarios.
+    '''
+        
+    close = 0
+    words = dict()
+	# TODO: implementar a funcionalidade aqui
+    file = open(fname)
+    
+    for line in file:
+
+        vetor = split_on_separators(line," ")
+        escore = int(line[0])
+        for word in vetor:
+            if(word in words):
+                words[word]['frequencia'] += 1
+                words[word]['escore'] += escore
+            else:
+                words[word] = {'frequencia': 1, 'escore': escore}
+        close += 1
+    file.close()
+    return words
+
+def GetStopWords():
+    stop_words =["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"]
+    return stop_words
+        
+def readTestSet(fname):
+    ''' Esta funcao le o arquivo contendo o conjunto de teste
+	retorna um vetor/lista de pares (escore,texto) dos
+	comentarios presentes no arquivo.
+    '''
+    count = 0
+    reviews = []
+    # TODO: implementar a funcionalidade
+    file = open(fname)
+    for line in file:
+        vetor = split_on_separators(line," ")
+        reviews.append([line[0],vetor])
+        count += 1
+        
+    file.close()
+    return reviews
+
+def computeSumSquaredErrors(review,words):
+    ''' Retorna o sentimento do comentario recebido como parametro.
+        O sentimento de um comentario e' a media dos escores de suas
+        palavras. Se uma palavra nao estiver no conjunto de palavras do
+        conjunto de treinamento, entao seu escore e' 2.
+        Review e' a parte textual de um comentario.
+        Words e' o dicionario com as palavras e seus escores medios no conjunto
+        de treinamento.
+    '''
+    total = 0.0
+    i = 0
+	# TODO: implementar a funcionalidade
+    for lista in review:
+        current_sentiment = 0.0
+        for p_for_check in lista[1]:
+            if(p_for_check in words):
+                current_sentiment += words[p_for_check]['escore']/words[p_for_check]['frequencia']
+            else:
+                current_sentiment += 2
+        current_sentiment = current_sentiment/len(lista[1])
+        total += (current_sentiment - int(lista[0]))**2
+    total = total/len(review)
+    return total
+    
+def main():
+  
+    # Os arquivos sao passados como argumentos da linha de comando para o programa
+    # Voce deve buscar mais informacoes sobre o funcionamento disso (e' parte do
+    # projeto).
+    
+    # A ordem dos parametros e' a seguinte: o primeiro e' o nome do arquivo
+    # com o conjunto de treinamento, em seguida o arquivo do conjunto de teste.
+    
+    if len(sys.argv) < 3:
+        print ('Numero invalido de argumentos')
+        print ('O programa deve ser executado como python sentiment_analysis.py <arq-treino> <arq-teste>')
+        sys.exit(0)
+
+    # Lendo conjunto de treinamento e computando escore das palavras
+    words = readTrainingSet(sys.argv[1])
+    
+    # Lendo conjunto de teste
+    reviews = readTestSet(sys.argv[2])
+    
+    # Inferindo sentimento e computando soma dos quadrados dos erros
+    #sse = computeSumSquaredErrors(reviews,words)
+    
+    sse = computeSumSquaredErrors(reviews,words)
+
+    #computeSumSquaredErrors(reviews,words)
+    
+    print ('A soma do quadrado dos erros e\': {0}'.format(sse))
+            
+
+if __name__ == '__main__':
+   main()
+    
+    
